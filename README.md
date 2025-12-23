a5241cc09a0fe54be6d397c464d846f0113d422b
Marcos <mgf108@alu.ua.es>
2025-12-23 23:10:45 +0100

    Added flag parsing and README.md

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# pword
-A lightweight password generator and evaluator, made in Rust
+# ptest
+A lightweight password generator and evaluator, made in Rust.
+
+## Create passwords
+
+Use this program in your shell to generate secure passwords:
+```shell
+ptest generate [length] [flags]
+```
+Examples:
+```shell
+ptest generate 50 -luds
+;lL-9hcIguOr/'>[AnTUsLwo]<:qUa]Wss`?I<'~W_;uvS1zoi
+```
+```shell
+ptest generate 50 -l
+nqrgvkfoflfcuahkygqbcwwovgrsotvfjmccwegyojcujxjvys
+```
+
+### Flags available
+`-l` -> lowercase characters (a-z)
+`-u` -> uppercase characters (A-Z)
+`-d` -> digits (0-9)
+`-s` -> symbols (!\"#$%&\'()*+,-./:;<=>?@\[\\\]^_\`{|}~)
+`-e` -> extended ASCII
+
+## Evaluate passwords (WIP)
+
+## Features
+
+- **Really fast**: uses `rand` with ChaCha20 CSPRNG.
+- **Secure**: generate strong and secure passwords.
+- **Simple CLI**: it goes directly to the point.
+- **Zero alloc**: precalculated char pool.
+
+## Installation
+
+### Compiling the code
+
+Firstly, clone the repository:
+```shell
+git clone https://github.com/ImLecus/ptest
+```
+
+In your terminal, run:
+```shell
+cd ptest
+cargo build
+cargo install --path .
+```
+
+If the installation was successful, the following command must show the program version:
+```shell
+ptest --version
+```
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ const DIGITS: u8         = 0b0000_0100;
 const SYMBOLS: u8        = 0b0000_1000;
 const EXTENDED_ASCII: u8 = 0b0001_0000;
 
+/// Returns a random character in a range determined by the flags
 fn get_random_char(flags: u8) -> char {
     let mut pool = String::new();
 
@@ -23,12 +24,37 @@ fn get_random_char(flags: u8) -> char {
     pool.chars().nth(index).unwrap()
 }
 
+/// Creates a password with a determined length
 fn create_password(len: usize, flags: u8) -> String {
     let mut password = String::new();
     for _ in 0..len {
         password.push(get_random_char(flags));
     }
     password
+}
+
+/// Parses the flags from format '-(flag)+' to a single byte
+fn parse_flags(flags_str: &str) -> u8 {
+    let mut flags : u8 = 0;
+
+    if flags_str.chars().nth(0).unwrap_or('\0') != '-' || flags_str.len() < 2 {
+        eprintln!("Unknown flag: {}", flags_str);
+        exit(1);
+    }
+    for c in flags_str.chars().skip(1) {
+        match c {
+            'l' => flags |= LOWER,
+            'u' => flags |= UPPER,
+            'd' => flags |= DIGITS,
+            's' => flags |= SYMBOLS,
+            'e' => flags |= EXTENDED_ASCII,
+            _ => {
+                eprintln!("Unknown flag: {}", c);
+                exit(1);
+            }
+        }
+    }
+    flags
 }
 
 fn main() {
@@ -48,17 +74,21 @@ fn main() {
                 exit(1);
             }
             let length = args[2].parse::<usize>().unwrap();
-            let flags : u8 = LOWER | UPPER | DIGITS | SYMBOLS;
+            let mut flags: u8 = LOWER | UPPER | DIGITS | SYMBOLS;
             if args.len() == 4 {
-                unimplemented!();
+                flags = parse_flags(&args[3]);
             }
             println!("{}", create_password(length, flags));
         },
         "evaluate" | "eval" | "e" => {
             unimplemented!();
         },
+        "--version" => {
+            println!("{}", env!("CARGO_PKG_VERSION"));
+        },
         _ => {
             eprintln!("Unknown command");
+            exit(1);
         }
     }
 }
